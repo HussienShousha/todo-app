43b1471e6c6079644561f9422d5a8c6b6dd008cc
آل شوشه <11410120226035@stud.cu.edu.eg>
2025-10-31 19:06:30 +0200

    some lines got added now in all todo file now

--- Routes/todos.routes.js
+++ Routes/todos.routes.js
@@ -22,7 +22,7 @@ router.get('/:id', auth ,getToDoByID);
 
 
 
-router.patch('/:id', auth ,restrictTo('admin') ,updateToDO)
+router.patch('/:id', auth ,restrictTo('user', 'admin') ,updateToDO);
 
 
 
