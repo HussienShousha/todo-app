2b1e2d6e5fcbe3235b5ceab2838bba6988a79777
آل شوشه <11410120226035@stud.cu.edu.eg>
2025-10-31 18:04:01 +0200

    some files got updated now

--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -12,16 +12,14 @@ const {
 
 const router = express.Router();
 
-
+router.get("/", getAllUsers);         
+router.post("/", saveUser);            
 router.post("/login", login);          
 router.post("/refreshToken", refreshToken);
-router.get("/", getAllUsers);         
 router.get("/edit/:id", getUpdatePage);
-router.post("/", saveUser);            
 
 router.get("/:id", getUserById);       
 router.patch("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
-//
 module.exports = router;
